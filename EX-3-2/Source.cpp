1d6a193512c9d0b1e0e6ba1b2a2d7a2cd92b431c
amarstang <35458474+amarstang@users.noreply.github.com>
2022-09-20 14:00:06 +0200

    EX-3.2

---
+++ EX-3-2/Source.cpp
@@ -0,0 +1,51 @@
+#pragma comment(lib, "RaspberryPI.lib")
+#include <stdio.h>
+#include <stdlib.h>
+#include <RaspberryDLL.h>
+
+//Red -> Red & Yellow -> Green -> Yellow -> Red
+//Led 4 Green, 5 Yellow, 6 Red.
+
+//Green->Yellow->Red
+void redLightTransition() { 
+	//Turns off green then turns on yellow and waits
+	ledOff(4);	//Green	 Off
+	ledOn(5);	//Yellow On
+	Wait(2000);	//Waits int value ms time
+
+	ledOff(5);	//Yellow	Off
+	ledOn(6);	//Red		On
+	Wait(2000);	
+}
+
+//Red -> Red & Yellow -> Green
+void GreenLightTransition() {
+	ledOn(5);	//Yellow	On
+	Wait(2000);
+
+	ledOff(6);	//Red		Off
+	ledOff(5);	//Yellow	Off
+	ledOn(4);	//Green		On
+	Wait(2000);
+}
+
+int main(void)
+{
+	if (!Open())
+	{
+		printf("Error with connection\n");
+		exit(1);
+	}
+
+	printf("Connected to Raspberry Pi\n");
+	
+	while (1) { //While loop: Extra for testing
+		redLightTransition();
+		Wait(3000);
+		GreenLightTransition();
+		Wait(3000);
+	}
+
+
+	return 0;
+}
